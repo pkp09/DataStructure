587a6b4277d260f5d6571b4a3a8e1fa0ec29ef79
pkp09 <pandeyprashant@live.in>
2020-05-06 13:46:47 +0530

    Commit for testing

--- src/com/prashant/codechef/problem/Codechef.java
+++ src/com/prashant/codechef/problem/Codechef.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 /* Name of the class has to be "Main" only if the class is public. */
 class Codechef
-{
+{ 
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// smallFactorials();
